cf99328ad61b9a9ce5f23345e0ee7cbdf70f8a6f
Madscba <madscba@gmail.com>
2023-05-09 22:49:10 +0200

    ready to train lunarlander tomorrow using AC

--- policy_gradient_methods/shell_scripts/run_config.sh
+++ policy_gradient_methods/shell_scripts/run_config.sh
@@ -4,10 +4,13 @@ python policy_gradient_methods\main.py --seed 3 --n_episodes 10000 --n_environme
 python policy_gradient_methods\main.py --seed 3 --n_episodes 1000 --n_environments 20 --learning_algorithm "REINFORCE" --grad_clipping 1 --entropy True
 
 
+python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.001 --env_name CartPole-v1
 
 
 python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.0001 --env_name LunarLanderContinuous-v2
 python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.001 --env_name LunarLanderContinuous-v2
+
+python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.0001 --env_name LunarLanderContinuous-v2 --hidden_size 64
 
 python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.01
 python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC"  --lr 0.0001
@@ -15,3 +18,14 @@ python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environmen
 
 #Best performing AC so far:
 python policy_gradient_methods\main.py --seed 3 --n_episodes 2000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.001
+
+#RUN TOMORROW:
+
+##decent performance
+python policy_gradient_methods\main.py --seed 3 --n_episodes 5000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.001 --env_name LunarLanderContinuous-v2
+python policy_gradient_methods\main.py --seed 3 --n_episodes 5000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.0001 --env_name LunarLanderContinuous-v2
+
+python policy_gradient_methods\main.py --seed 3 --n_episodes 5000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.0001 --env_name LunarLanderContinuous-v2 --hidden_size 64
+
+##pretty bad performance
+python policy_gradient_methods\main.py --seed 3 --n_episodes 5000 --n_environments 1 --learning_algorithm "AC" --grad_clipping 1 --entropy True --lr 0.001 --env_name LunarLanderContinuous-v2 --hidden_size 64
