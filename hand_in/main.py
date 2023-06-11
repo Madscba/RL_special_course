0a795fae13a353db3a73738a7cdc5ad6d9a8637c
Madscba <Madscba@gmail.com>
2023-06-11 20:03:45 +0200

    small changes to main.py

--- hand_in/main.py
+++ hand_in/main.py
@@ -4,10 +4,8 @@ import numpy as np
 import os
 import sys
 
-# Get the current script's directory
+# Needed to run the main from the terminal.
 script_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Add the repository's root directory to the Python path
 repo_root = os.path.abspath(os.path.join(script_dir, ".."))
 sys.path.append(repo_root)
 
@@ -24,7 +22,6 @@ if __name__ == "__main__":
     e, state = get_envs(env_id=p.args.env_name, num_envs=p.args.n_env)
     l = get_logger(p)
     a = get_agent(p, e)
-
     best_avg_epi_score = -9999
 
     for frame_count in tqdm(range(p.args.n_steps), desc=f"Training"):
@@ -49,25 +46,16 @@ if __name__ == "__main__":
         )
 
         state = new_state
-
         l.log_step(reward)
 
         if terminated or l.current_episode_frame_counter > 1000:
-            # state, _ = e.reset()
             terminated = False
             l.log_episode()
-            # if l.episode_counter % 200 == 0:
-            #     evaluate_agent(a, p.args.env_name, num_episodes=3)
-
-        # if frame_count % (p.args.n_steps//20) == 0 and (p.args.n_steps/1.25) < frame_count:
-        # evaluate_agent(a, p.args.env_name, num_episodes=2, render=True)
 
         avg_epi_score = l.get_avg_reward_last_episodes()
         if avg_epi_score > best_avg_epi_score:
             best_avg_epi_score = avg_epi_score
             a.save_models()
-            # if l.episode_counter % 50 == 0:
-            #     evaluate_agent(a, p.args.env_name, num_episodes=5, render=False, best_model=True)
 
     l.plot_epi_rewards()
     l.plot_step_rewards()
